379d7fbdf60e0347591f5704fada2bf9681d9c98
naiiiden <na4ko359@gmail.com>
2024-07-05 23:59:55 +0300

    header nav

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,8 +5,19 @@ export default function Header() {
     console.log(data);
 
     return (
-        <header className="fixed top-0 text-white p-4 font-semibold text-lg">
+        <header className="fixed top-0 text-white p-4 font-semibold text-lg flex justify-between w-full">
             <Link href="/">{data.name}</Link>
+            <nav>
+                <ul className="text-end">
+                    {data.links.map((link) => 
+                        <li key={link.name}>
+                            <Link href={link.url}>
+                                {link.name}
+                            </Link>
+                        </li>
+                    )}
+                </ul>
+            </nav>
         </header>
     )
 }
