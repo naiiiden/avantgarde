291d4baa9c4eda62c4a92bd6da270dc0d097bb9b
naiiiden <na4ko359@gmail.com>
2024-07-05 23:01:22 +0300

    bg img & hero text

--- app/page.js
+++ app/page.js
@@ -4,9 +4,8 @@ export default function Home() {
   console.log(data);
 
   return (
-    <div>
-      <h1>hi</h1>
-
+    <div style={{ backgroundImage: `url(${data[0].image})` }} className="bg-black h-screen bg-cover bg-center grid items-end">
+      <h1 className="text-white font-semibold text-xl p-4 max-w-72 ml-auto lg:text-3xl lg:max-w-[25rem]">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
     </div>
   )
 }
