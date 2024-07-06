abbca4eb1694b1392ff9a9243ba39900ae18278b
naiiiden <na4ko359@gmail.com>
2024-07-06 17:23:09 +0300

    add logo svg

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import data from "./data.json";
+import Image from "next/image";
+
+import avantgarde from "@/public/avantgarde.svg";
 
 export default function Header() {
     console.log(data);
 
     return (
-        <header className="fixed top-0 text-white p-4 font-semibold text-lg flex justify-between w-full">
-            <Link href="/">{data.name}</Link>
+        <header className="fixed top-0 text-white p-4 font-semibold text-lg flex flex-col justify-between w-full">
+            {/* <Link href="/">{data.name}</Link> */}
+            <Image className="w-full" src={avantgarde} alt={data.name}/>
             <nav>
                 <ul className="text-end">
                     {data.links.map((link) => 
