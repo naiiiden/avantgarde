10bd24f2c8782c3c5b76ea2e401475e792b4ae90
naiiiden <na4ko359@gmail.com>
2024-07-06 20:01:59 +0300

    scale logo down on scroll down and vice versa

--- app/page.js
+++ app/page.js
@@ -4,8 +4,13 @@ export default function Home() {
   // console.log(data);
 
   return (
-    <div style={{ backgroundImage: `url(${data[0].image})` }} className="bg-black h-screen bg-cover bg-center grid items-end">
-      <h1 className="text-white font-semibold text-xl p-4 max-w-72 ml-auto lg:text-3xl lg:max-w-[25rem]">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
-    </div>
+    <>
+      <div style={{ backgroundImage: `url(${data[0].image})` }} className="bg-black h-screen bg-cover bg-center grid items-end">
+        <h1 className="text-white font-semibold text-xl p-4 max-w-72 ml-auto lg:text-3xl lg:max-w-[25rem]">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
+      </div>
+      <div className="h-screen bg-green-500">
+
+      </div>
+    </>
   )
 }
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,18 +1,34 @@
+"use client";
 import Link from "next/link";
 import data from "./data.json";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 import avantgarde from "@/public/avantgarde.svg";
 
 export default function Header() {
-    console.log(data);
+    const [imageWidth, setImageWidth] = useState("100%");
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const newWidth = Math.max(50, 100 - window.scrollY / 20) + "%";
+            setImageWidth(newWidth);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    // console.log(data);
 
     return (
-        <header className="fixed top-0 text-white p-4 font-semibold text-lg flex flex-col justify-between w-full">
-            <Link href="/">
+        <header className="fixed top-0 text-white p-4 font-semibold text-lg flex flex-wrap justify-between w-full">
+            <Link style={{ width: imageWidth }} href="/" className="w-full">
                 <Image className="w-full" priority src={avantgarde} alt={data.name}/>
             </Link>
-            <nav>
+            <nav className="ml-auto">
                 <ul className="text-end">
                     {data.links.map((link) => 
                         <li key={link.name}>
