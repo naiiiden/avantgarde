783d6d572f61598290009e84b4b704601f616dbe
naiiiden <na4ko359@gmail.com>
2024-07-07 15:02:11 +0300

    random bg from collection on load

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import data from "@/public/chairs.json";
 
 export default function Home() {
-  // console.log(data);
+  console.log(data);
+
+  const randomItemFromCollection = data[(Math.floor(Math.random() * data.length))];
+  console.log(1, randomItemFromCollection);
 
   return (
     <>
-      <div style={{ backgroundImage: `url(${data[5].image})` }} className="bg-black h-screen bg-cover bg-center grid items-end">
+      <div style={{ backgroundImage: `url(${randomItemFromCollection.image})` }} className="bg-black h-screen bg-cover bg-center grid items-end">
         <h1 className="text-white font-semibold text-xl p-4 max-w-72 ml-auto lg:text-3xl lg:max-w-[25rem]">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
       </div>
       <div className="h-screen bg-green-500">
