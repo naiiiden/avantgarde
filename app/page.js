4c20b0e79aadbcd3d7e0374770abea9cd50ed297
naiiiden <na4ko359@gmail.com>
2024-07-07 14:57:28 +0300

    add new chairs to .json

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <>
-      <div style={{ backgroundImage: `url(${data[0].image})` }} className="bg-black h-screen bg-cover bg-center grid items-end">
+      <div style={{ backgroundImage: `url(${data[5].image})` }} className="bg-black h-screen bg-cover bg-center grid items-end">
         <h1 className="text-white font-semibold text-xl p-4 max-w-72 ml-auto lg:text-3xl lg:max-w-[25rem]">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
       </div>
       <div className="h-screen bg-green-500">
