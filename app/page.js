0774314f0f2427b38e726a21012c87fa36536648
naiiiden <na4ko359@gmail.com>
2024-07-07 22:46:24 +0300

    temporary items layout

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
 import data from "@/public/chairs.json";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   console.log(data);
@@ -11,11 +13,13 @@ export default function Home() {
       <div style={{ backgroundImage: `url(${randomItemFromCollection.image})` }} className="bg-black h-screen bg-cover bg-center grid items-end">
         <h1 className="text-white font-semibold text-xl p-4 max-w-72 ml-auto lg:text-3xl lg:max-w-[25rem]">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
       </div>
-      <div className="h-screen bg-green-500">
-        <ul className="grid grid-cols-2">
+      <div className="">
+        <ul className="p-4 grid grid-cols-2 gap-4 lg:grid-cols-4">
           {data.map((item, index) =>
             <li key={index}>
-              <img src={item.image}/>
+              <Link href={item.name}>
+                <Image className="w-full h-full aspect-square" src={item.image} width={500} height={500} alt={item.name}/>
+              </Link>
             </li>
           )}
         </ul>
