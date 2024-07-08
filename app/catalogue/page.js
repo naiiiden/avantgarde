fe9759434c331ade9266dcf2a921087c49324bb4
naiiiden <na4ko359@gmail.com>
2024-07-08 23:10:42 +0300

    catalogue render data

--- app/catalogue/page.js
+++ app/catalogue/page.js
@@ -1,7 +1,19 @@
-export default function Page({ params }) {
-    console.log(2, params.slug);
+import Image from "next/image";
+import Link from "next/link";
+import data from "@/public/chairs.json";
 
+export default function Page() {
     return (
-        <h1>catalogue</h1>
+        <main>
+            <ul className="p-4 grid grid-cols-2 gap-4 lg:grid-cols-4">
+                {data.map((item, index) =>
+                    <li key={index}>
+                        <Link href={item.name}>
+                            <Image className="w-full h-full aspect-square" src={item.image} width={500} height={500} alt={item.name}/>
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </main>
     )
 }
--- app/page.js
+++ app/page.js
@@ -13,17 +13,6 @@ export default function Home() {
       <div style={{ backgroundImage: `url(${randomItemFromCollection.image})` }} className="bg-black h-screen bg-cover bg-center grid items-end">
         <h1 className="text-white font-semibold text-xs p-4 ml-auto lg:text-xs">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
       </div>
-      {/* <div className="">
-        <ul className="p-4 grid grid-cols-2 gap-4 lg:grid-cols-4">
-          {data.map((item, index) =>
-            <li key={index}>
-              <Link href={item.name}>
-                <Image className="w-full h-full aspect-square" src={item.image} width={500} height={500} alt={item.name}/>
-              </Link>
-            </li>
-          )}
-        </ul>
-      </div> */}
     </>
   )
 }
