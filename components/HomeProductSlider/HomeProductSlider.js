17c9bea5fe6fa9a73d43b7afcbd61c9c2b76e869
naiiiden <na4ko359@gmail.com>
2024-07-09 18:31:53 +0300

    slider custom cursor setup

--- components/HomeProductSlider/HomeProductSlider.js
+++ components/HomeProductSlider/HomeProductSlider.js
@@ -1,17 +1,24 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { register } from "swiper/element/bundle";
-import Image from "next/image";
-
 import data from "@/public/chairs.json";
 
 export default function HomeProductSilder() {
     const sliderRef = useRef(null);
-
-    console.log(sliderRef);
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
     useEffect(() => {
         register();
+
+        const swiper = sliderRef.current.swiper;
+        const handleSlideChange = () => {
+            setCurrentSlideIndex(swiper.realIndex);
+        };
+
+        swiper.on('slideChange', handleSlideChange);
+        return () => {
+            swiper.off('slideChange', handleSlideChange);
+        };
     }, []);
 
     return (
@@ -24,16 +31,9 @@ export default function HomeProductSilder() {
                 )}
             </swiper-container>
             <div className="z-10">
-                <button aria-label="Prev slide" className="border fixed w-1/2 left-0 top-0 h-full" onClick={() => sliderRef.current.swiper.slidePrev()}>
-                    <svg width="18" height="10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5 8.9a1 1 0 0 1-1.414 0L.395 5.708a1 1 0 0 1-.001-1.415L3.586 1.1A1 1 0 0 1 5 2.515L3.515 4H17a1 1 0 1 1 0 2H3.515L5 7.485A1 1 0 0 1 5 8.9Z" fill="#000"></path>
-                    </svg>
-                </button>
-                <button aria-label="Next slide" className="border fixed w-1/2 right-0 top-0 h-full rotate-180" onClick={() => sliderRef.current.swiper.slideNext()}>
-                    <svg width="18" height="10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5 8.9a1 1 0 0 1-1.414 0L.395 5.708a1 1 0 0 1-.001-1.415L3.586 1.1A1 1 0 0 1 5 2.515L3.515 4H17a1 1 0 1 1 0 2H3.515L5 7.485A1 1 0 0 1 5 8.9Z" fill="#000"></path>
-                    </svg>
-                </button>
+                <button aria-label="Prev slide" className="border fixed w-1/2 left-0 top-0 h-full rotate-180" onClick={() => sliderRef.current.swiper.slidePrev()}></button>
+                <button aria-label="Next slide" className="border fixed w-1/2 right-0 top-0 h-full" onClick={() => sliderRef.current.swiper.slideNext()}></button>
+                <div className="">{currentSlideIndex + 1} / {data.length}</div>
             </div>
         </>
     )
