b6a70b3a07dc97d61875aa8b154adc3e7f6de9e2
naiiiden <na4ko359@gmail.com>
2024-07-09 23:10:16 +0300

    add link to view product

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <div className="bg-black h-screen bg-cover bg-center flex flex-col-reverse">
-        <h1 className="text-white font-semibold text-xs p-4 ml-auto relative z-10 lg:text-sm 2xl:text-base">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
+        <h1 className="text-white font-semibold text-xs p-4 ml-auto relative z-20 lg:text-sm 2xl:text-base">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
         <HomeProductSilder/>
       </div>
     </>
--- components/HomeProductSlider/HomeProductSlider.js
+++ components/HomeProductSlider/HomeProductSlider.js
@@ -1,12 +1,15 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import { register } from "swiper/element/bundle";
+import Link from "next/link";
 import data from "@/public/chairs.json";
 
 export default function HomeProductSilder() {
     const sliderRef = useRef(null);
+    const controlsRef = useRef(null);
     const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
     const [cursorPosition, setCursorPosition] = useState({ top: 0, left: 0 });
+    const [productLinkHref, setProductLinkHref] = useState(null);
 
     const updateCursorPosition = (e) => {
         const rect = e.currentTarget.getBoundingClientRect();
@@ -22,6 +25,8 @@ export default function HomeProductSilder() {
         const swiper = sliderRef.current.swiper;
         const handleSlideChange = () => {
             setCurrentSlideIndex(swiper.realIndex);
+            setProductLinkHref(data[swiper.realIndex].name);
+            console.log(swiper.realIndex);
         };
 
         swiper.on('slideChange', handleSlideChange);
@@ -32,17 +37,20 @@ export default function HomeProductSilder() {
 
     return (
         <>
-            <swiper-container ref={sliderRef} class="grid w-full fixed" loop>
+            <swiper-container ref={sliderRef} class="grid w-full fixed z-10" loop>
                 {data.map((item, index) => 
                     <swiper-slide key={index}>
                         <div style={{ backgroundImage: `url(${item.image})` }} className="h-screen bg-cover bg-center"></div>
                     </swiper-slide>
                 )}
             </swiper-container>
-            <div className="z-10 fixed inset-0 group" onMouseMove={updateCursorPosition}>
+            <div ref={controlsRef} className="z-10 fixed inset-0 group" onMouseMove={updateCursorPosition}>
                 <button aria-label="Prev slide" className="cursor-none fixed w-1/2 left-0 top-0 h-full rotate-180" onClick={() => sliderRef.current.swiper.slidePrev()}></button>
                 <button aria-label="Next slide" className="cursor-none fixed w-1/2 right-0 top-0 h-full" onClick={() => sliderRef.current.swiper.slideNext()}></button>
-                <div style={{ ...cursorPosition }} className="hidden group-hover:block absolute -z-10 font-semibold text-5xl tracking-tighter min-w-36 text-white">{currentSlideIndex + 1} / {data.length}</div>
+                <div style={{ ...cursorPosition }} className="hidden group-hover:block absolute -z-10 font-semibold text-5xl tracking-tighter min-w-40 text-white">{currentSlideIndex + 1} / {data.length}</div>
+                {productLinkHref !== null && <Link href={productLinkHref} className="absolute font-semibold z-40 bottom-12 right-4 py-2 px-8 bg-black text-white">
+                    View product
+                </Link>}
             </div>
         </>
     )
