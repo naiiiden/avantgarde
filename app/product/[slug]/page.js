2c86f029cb092458c49637df7a02344076c3e000
naiiiden <na4ko359@gmail.com>
2024-07-11 17:29:05 +0300

    get current product

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -1,5 +1,8 @@
+import data from "@/public/chairs.json";
+
 export default function Page({ params }) {
-    console.log("product params:", params.slug);
+    const currentProduct = data.find((item) => params.slug === item.urlHandle );
+    console.log('current item', currentProduct);
 
     return (
         <h1>{params.slug}</h1>
