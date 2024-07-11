33ac39beedb4b375582751c2756dcd191b5748fb
naiiiden <na4ko359@gmail.com>
2024-07-11 23:39:49 +0300

    product notfound

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
 import data from "@/public/chairs.json";
 
 export default function Page({ params }) {
     const currentProduct = data.find((item) => params.slug === item.urlHandle );
     console.log('current item', currentProduct);
+
+    if (!data.map(item => item.urlHandle).includes(params.slug)) {
+        notFound();
+    }
 
     return (
         <h1>{params.slug}</h1>
