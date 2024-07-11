bb80daf7bbcca0d99f4600a4a13920404f1342ab
naiiiden <na4ko359@gmail.com>
2024-07-11 13:46:32 +0300

    nav reduce font-size on large screen

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <div className="bg-black h-screen bg-cover bg-center max-lg:flex flex-col-reverse">
-        <h1 className="text-white font-semibold text-xs p-4 ml-auto z-20 lg:text-sm lg:fixed lg:bottom-0 lg:right-0 2xl:text-base">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
+        <h1 className="text-white font-semibold text-xs p-4 ml-auto z-20 lg:text-sm lg:fixed lg:bottom-0 lg:right-0">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
         <HomeProductSilder/>
       </div>
     </>
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,7 +5,7 @@ import HeaderLogo from "./HeaderLogo";
 
 export default function Header({ className }) {
     return (
-        <header className={`${className} fixed z-20 top-0 text-white p-4 font-semibold text-lg flex flex-wrap justify-between gap-4 w-full`}>
+        <header className={`${className} fixed z-20 top-0 text-white p-4 font-semibold flex flex-wrap justify-between gap-4 w-full`}>
             <HeaderLogo/>
             <nav className="ml-auto">
                 <ul className="text-end grid gap-3">
