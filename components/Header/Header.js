43eb83413738392a541e89178daba87608e361ba
naiiiden <na4ko359@gmail.com>
2024-07-13 17:25:18 +0300

    headerlogo improvements > use pathname, remove prop drilling

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,10 +3,10 @@ import data from "./data.json";
 
 import HeaderLogo from "./HeaderLogo";
 
-export default function Header({ className, maxWidth }) {
+export default function Header({ className }) {
     return (
         <header className={`${className} fixed z-20 top-0 text-white p-4 font-semibold flex flex-wrap justify-between gap-4 w-full`}>
-            <HeaderLogo maxWidth={maxWidth}/>
+            <HeaderLogo/>
             <nav className="ml-auto">
                 <ul className="text-end grid gap-3">
                     {data.links.map((link) => 
