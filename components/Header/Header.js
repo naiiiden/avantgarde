f526744c5bbc18015c7ff6c56066ca27874a0f39
naiiiden <na4ko359@gmail.com>
2024-07-13 17:57:39 +0300

    header nav style update

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,10 +5,10 @@ import HeaderLogo from "./HeaderLogo";
 
 export default function Header({ className }) {
     return (
-        <header className={`${className} fixed z-20 top-0 text-white p-4 font-semibold flex flex-wrap justify-between gap-4 w-full`}>
+        <header className={`${className} fixed z-20 top-0 text-white p-4 font-semibold flex flex-wrap justify-between gap-3 w-full`}>
             <HeaderLogo/>
             <nav className="ml-auto">
-                <ul className="text-end grid gap-3">
+                <ul className="text-end flex gap-4">
                     {data.links.map((link) => 
                         <li key={link.name}>
                             <Link href={link.url} className="block leading-none">
