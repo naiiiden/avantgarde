858292ca6d6ad1dafa4e8de8c394ced0e2e02745
naiiiden <na4ko359@gmail.com>
2024-07-14 14:32:06 +0300

    sort input state effect

--- app/catalogue/page.js
+++ app/catalogue/page.js
@@ -11,23 +11,23 @@ export default function Page() {
                 <form className="pb-4 flex flex-wrap gap-x-2 justify-end">
                     <p>Sort by:</p>
                     <label>
-                        <input name="sort-by" id="alphabeticalAtoZ" className="absolute" type="radio"/>
-                        alphabetical, A-Z
+                        <input name="sort-by" id="alphabeticalAtoZ" className="peer opacity-0 absolute" type="radio"/>
+                        <span className="peer-checked:underline peer-hover:underline decoration-2 peer-focus:outline ">alphabetical, A-Z</span>
                         <span aria-hidden="true">,</span>
                     </label>
                     <label>
-                        <input name="sort-by" id="alphabeticalZtoA" className="absolute" type="radio"/>
-                        alphabetical, Z-A
+                        <input name="sort-by" id="alphabeticalZtoA" className="peer opacity-0 absolute" type="radio"/>
+                        <span className="peer-checked:underline peer-hover:underline decoration-2 peer-focus:outline ">alphabetical, Z-A</span>
                         <span aria-hidden="true">,</span>
                     </label>
                     <label>
-                        <input name="sort-by" id="priceAscending" className="absolute" type="radio"/>
-                        price ascending
+                        <input name="sort-by" id="priceAscending" className="peer opacity-0 absolute" type="radio"/>
+                        <span className="peer-checked:underline peer-hover:underline decoration-2 peer-focus:outline ">price ascending</span>
                         <span aria-hidden="true">,</span>
                     </label>
                     <label>
-                        <input name="sort-by" id="priceDescending" className="absolute" type="radio"/>
-                        price descending
+                        <input name="sort-by" id="priceDescending" className="peer opacity-0 absolute" type="radio"/>
+                        <span className="peer-checked:underline peer-hover:underline decoration-2 peer-focus:outline ">price descending</span>
                     </label>
                 </form>
             </div>
