2853e02528b29ba096708a0485bc498643282038
naiiiden <na4ko359@gmail.com>
2024-07-14 22:53:41 +0300

    refactor headergutter, add resize eventlistener

--- components/Header/HeaderGutter.js
+++ components/Header/HeaderGutter.js
@@ -4,11 +4,19 @@ import { useState, useEffect } from "react";
 export default function HeaderGutter() {
     const [headerHeight, setHeaderHeight] = useState(0);
 
-    useEffect(() => {
+    const updateHeaderHeight = () => {
         const header = document.querySelector('header');
         if (header) {
-            const height = header.offsetHeight;
-            setHeaderHeight(height);
+            setHeaderHeight(header.offsetHeight);
+        }
+    };
+
+    useEffect(() => {
+        updateHeaderHeight();
+
+        window.addEventListener("resize", updateHeaderHeight);
+        return () => {
+            window.removeEventListener('resize', updateHeaderHeight);
         }
     }, [])
     
