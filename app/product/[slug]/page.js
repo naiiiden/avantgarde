a966883b7ad7a0aa98b127eacf18ead1918e9a78
naiiiden <na4ko359@gmail.com>
2024-07-16 22:26:07 +0300

    add to cart btn

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -17,14 +17,14 @@ export default function Page({ params }) {
             <Header className={"fixed invert"}/>
             <HeaderGutter/>
             <main className="px-4 pb-4 flex flex-col gap-4 lg:flex-row">
-                <Image className="lg:w-9/12 xl:w-4/5 2xl:w-5/6" src={currentProduct.image} width={1500} height={1500} alt=""/>
+                <Image className="lg:w-9/12 xl:w-4/5 2xl:w-5/6" src={currentProduct.image} width={3000} height={3000} alt=""/>
                 <div>
                     <div className="sticky top-[51.25px]">
-                        <h1 className="font-semibold">{currentProduct.name}</h1>
-                        <p>by {currentProduct.creator}</p>
-                        <p>Material(s): {currentProduct.medium}</p>
-                        <p>Dimensions: {currentProduct.dimensions}</p>
-                        <p className="font-semibold">€{currentProduct.price}</p>
+                        <h1><span className="font-semibold">{currentProduct.name}</span> by {currentProduct.creator}</h1>
+                        <p><span className="font-semibold">Material(s):</span> {currentProduct.medium}</p>
+                        <p><span className="font-semibold">Dimensions: </span>{currentProduct.dimensions}</p>
+                        <p><span className="font-semibold">Price:</span> €{currentProduct.price}</p>
+                        <button className="mt-4 font-semibold bg-black text-white w-full p-2">Add to cart</button>
                     </div>
                 </div>
             </main>
