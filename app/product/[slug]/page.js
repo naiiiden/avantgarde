ae92ad68fda113116a16b72ac666346639f37d6e
naiiiden <na4ko359@gmail.com>
2024-07-16 19:34:19 +0300

    product page add img, text

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -1,5 +1,8 @@
 import { notFound } from "next/navigation";
 import data from "@/public/chairs.json";
+import Header from "@/components/Header/Header";
+import HeaderGutter from "@/components/Header/HeaderGutter";
+import Image from "next/image";
 
 export default function Page({ params }) {
     const currentProduct = data.find((item) => params.slug === item.urlHandle );
@@ -10,6 +13,19 @@ export default function Page({ params }) {
     }
 
     return (
-        <h1>{params.slug}</h1>
+        <>
+            <Header className={"fixed invert"}/>
+            <HeaderGutter/>
+            <main className="px-4">
+                <Image className="" src={currentProduct.image} width={1500} height={1500} alt=""/>
+                <div className="fixed bottom-0 right-0 px-4">
+                    <h1 className="font-semibold">{currentProduct.name}</h1>
+                    <p>by {currentProduct.creator}</p>
+                    <p>Material(s): {currentProduct.medium}</p>
+                    <p>Dimensions: {currentProduct.dimensions}</p>
+                    <p className="font-semibold">€{currentProduct.price}</p>
+                </div>
+            </main>
+        </>
     )
 }
