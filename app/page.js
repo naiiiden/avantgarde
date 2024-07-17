2121385bf86e3931ad6b08e61b502146af2ec69e
naiiiden <na4ko359@gmail.com>
2024-07-17 17:15:46 +0300

    homepage add main tag

--- app/page.js
+++ app/page.js
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <>
       <Header className={"fixed"}/>
-      <div className="bg-black h-screen bg-cover bg-center max-lg:flex flex-col-reverse">
+      <main className="bg-black h-screen bg-cover bg-center max-lg:flex flex-col-reverse">
         <h1 className="text-white font-semibold text-xs p-4 ml-auto z-20 lg:text-sm lg:fixed lg:bottom-0 lg:right-0">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
         <HomeProductSilder/>
-      </div>
+      </main>
     </>
   )
 }
