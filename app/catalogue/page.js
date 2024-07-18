942a3f6094ceb56d085e7240d4f5c3a08eca7bf7
naiiiden <na4ko359@gmail.com>
2024-07-18 15:50:11 +0300

    fetch test

--- app/catalogue/page.js
+++ app/catalogue/page.js
@@ -1,6 +1,27 @@
+import 'dotenv/config';
 import ProductsList from "@/components/ProductsList/ProductsList";
 
-export default function Page({ searchParams }) {
+async function getData() {
+    const res = await fetch('http://localhost:1337/api/products?populate=productImage', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.STRAPI_API_KEY}`,
+        },
+    });
+
+    if (!res.ok) {
+        throw new Error('failed to fetch data');
+    }
+
+    return res.json();
+}
+
+export default async function Page({ searchParams }) {
+    const data = await getData();
+
+    console.log('fetched data: ', data[0]?.attributes);
+
     return (
         <main className="main-reveal px-4 pb-4 font-medium">
             <h1 className="sr-only">Catalogue</h1>
