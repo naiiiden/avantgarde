f0f00165159a1d610414301335c03682c426eb77
naiiiden <na4ko359@gmail.com>
2024-07-19 19:41:01 +0300

    redesign homepage

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import Header from "@/components/Header/Header";
 export default function Home() {
   return (
     <>
-      <Header className={"fixed"}/>
-      <main className="bg-black h-screen bg-cover bg-center max-lg:flex flex-col-reverse">
-        <h1 className="text-white font-semibold text-xs p-4 ml-auto z-20 lg:text-sm lg:fixed lg:bottom-0 lg:right-0">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
+      <Header className={"invert fixed"}/>
+      <main className="h-screen bg-cover bg-center max-lg:flex flex-col-reverse">
+        <h1 className="font-semibold text-xs p-4 ml-auto z-20 lg:text-sm lg:fixed lg:bottom-0 lg:right-0">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
         <HomeProductSilder/>
       </main>
     </>
--- components/HomeProductSlider/HomeProductSlider.js
+++ components/HomeProductSlider/HomeProductSlider.js
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { register } from "swiper/element/bundle";
 import Link from "next/link";
 import data from "@/public/chairs.json";
+import Image from "next/image";
 
 export default function HomeProductSlider() {
     const sliderRef = useRef(null);
@@ -50,20 +51,20 @@ export default function HomeProductSlider() {
 
     return (
         <>
-            <swiper-container ref={sliderRef} class="grid w-full fixed z-10 lg:static" loop>
+            <swiper-container ref={sliderRef} class="grid w-full fixed z-10" loop>
                 {data.map((item, index) => 
-                    <swiper-slide key={index}>
-                        <div style={{ backgroundImage: `url(${item.image})` }} className="h-screen lg:h-[125vh] xl:h-[175vh] 2xl:h-[200vh] xl:bg-[length:100%_100%] bg-cover bg-center"></div>
+                    <swiper-slide class="flex items-center h-screen" key={index}>
+                        <Image className="max-w-[30%] mx-auto" src={item.image} width={1500} height={1500} alt=""/>
                     </swiper-slide>
                 )}
             </swiper-container>
             <div ref={controlsRef} className="hidden lg:block z-10 fixed inset-0 group" onMouseMove={updateCursorPosition}>
                 <button aria-label="Prev slide" className="cursor-none fixed w-1/2 left-0 top-0 h-full rotate-180" onClick={() => sliderRef.current.swiper.slidePrev()}></button>
                 <button aria-label="Next slide" className="cursor-none fixed w-1/2 right-0 top-0 h-full" onClick={() => sliderRef.current.swiper.slideNext()}></button>
-                <div style={{ ...cursorPosition }} className="hidden group-hover:block absolute -z-10 font-semibold text-5xl tracking-tighter min-w-40 text-white">{currentSlideIndex + 1} / {data.length}</div>
+                <div style={{ ...cursorPosition }} className="hidden group-hover:block absolute -z-10 font-semibold text-5xl tracking-tighter min-w-40">{currentSlideIndex + 1} / {data.length}</div>
             </div>
             <div className="fixed z-40 max-lg:w-full flex items-center justify-between bottom-20 sm:bottom-16 md:bottom-12 lg:bottom-[3.25rem] right-0 px-4">
-                <div className="font-semibold text-lg tracking-tighter text-white text-end lg:hidden">{currentSlideIndex + 1} / {data.length}</div>
+                <div className="font-semibold text-lg tracking-tighter text-end lg:hidden">{currentSlideIndex + 1} / {data.length}</div>
                 {productLinkHref !== null && <Link href={`product/${productLinkHref}`} className="font-semibold py-2 px-8 bg-black text-white">
                     View product
                 </Link>}
