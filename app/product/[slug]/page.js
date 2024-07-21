eb77fde8359e5565ad42eb168a062d95b385cd91
naiiiden <na4ko359@gmail.com>
2024-07-21 23:15:21 +0300

    change port

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -3,7 +3,7 @@ import HeaderGutter from "@/components/Header/HeaderGutter";
 import Image from "next/image";
 
 async function getData(slug) {
-    const res = await fetch(`http://localhost:1337/api/products?filters[productUrlHandle][$eq]=${slug}&populate=productImage`, {
+    const res = await fetch(`http://localhost:3333/api/products?filters[productUrlHandle][$eq]=${slug}&populate=productImage`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -37,7 +37,7 @@ export default async function Page({ params }) {
             <main className="main-reveal px-4 pb-4 flex flex-col gap-4 lg:flex-row">
                 {currentProduct && currentProduct.data.length > 0 ?
                     <>
-                        <Image className="lg:w-9/12 xl:w-4/5 2xl:w-5/6" src={`http://localhost:1337${currentProduct.data[0].attributes.productImage.data.attributes.url}`} width={3000} height={3000} alt=""/>
+                        <Image className="lg:w-9/12 xl:w-4/5 2xl:w-5/6" src={`http://localhost:3333${currentProduct.data[0].attributes.productImage.data.attributes.url}`} width={3000} height={3000} alt=""/>
                         <div>
                             <div className="sticky top-[51.25px]">
                                 <h1><span className="font-semibold">{currentProduct.data[0].attributes.productName}</span> by {currentProduct.data[0].attributes.productCreator}</h1>
--- components/ProductsList/ProductsList.js
+++ components/ProductsList/ProductsList.js
@@ -49,7 +49,7 @@ export default function ProductsList({ searchParams, data }) {
                                 onMouseLeave={() => setHoveredIndex(null)}
                                 onFocus={() => setHoveredIndex(index)}
                                 onBlur={() => setHoveredIndex(null)}>
-                                <Image className="" src={`http://localhost:1337${item.attributes.productImage.data.attributes.url}`} width={1500} height={1500} alt="" />
+                                <Image className="" src={`http://localhost:3333${item.attributes.productImage.data.attributes.url}`} width={1500} height={1500} alt="" />
                                 <h2 className="top-0 p-1.5 text-sm">{item.attributes.productName}</h2>
                             </Link>
                         </li>
@@ -76,7 +76,7 @@ export default function ProductsList({ searchParams, data }) {
                                     onMouseLeave={() => setHoveredIndex(null)}
                                     onFocus={() => setHoveredIndex(index)}
                                     onBlur={() => setHoveredIndex(null)}>
-                                    <Image className="max-w-24 sm:max-w-32 lg:max-w-40" src={`http://localhost:1337${item.attributes.productImage.data.attributes.url}`} width={1500} height={1500} alt="" />
+                                    <Image className="max-w-24 sm:max-w-32 lg:max-w-40" src={`http://localhost:3333${item.attributes.productImage.data.attributes.url}`} width={1500} height={1500} alt="" />
                                     <div className="p-1.5 flex gap-2 md:gap-4 xl:gap-6 2xl:gap-8 flex-grow">
                                         <h2 className="w-3/5 sm:w-1/3 md:w-1/4 lg:w-1/5">{item.attributes.productName}</h2>
                                         <p className="hidden sm:block sm:w-1/3 md:w-1/4 lg:w-1/5">{item.attributes.productCreator}</p>
