9e82278b84b7c75c9c1c09d75fdc4321bc76b959
naiiiden <na4ko359@gmail.com>
2024-07-27 09:13:40 +0300

    homeproductslider rework

--- components/HomeProductSlider/HomeProductSlider.js
+++ components/HomeProductSlider/HomeProductSlider.js
@@ -9,7 +9,6 @@ export default function HomeProductSlider({ data }) {
     const controlsRef = useRef(null);
     const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
     const [cursorPosition, setCursorPosition] = useState({ top: -100, left: 0 });
-    const [productLinkHref, setProductLinkHref] = useState(data[0].attributes.urlHandle);
 
     const updateCursorPosition = (e) => {
         const rect = e.currentTarget.getBoundingClientRect();
@@ -29,14 +28,12 @@ export default function HomeProductSlider({ data }) {
             swiper.slideToLoop(localStorageCurrentSlideIndex, 0);
             
             setCurrentSlideIndex(localStorageCurrentSlideIndex);
-            setProductLinkHref(data[localStorageCurrentSlideIndex].attributes.urlHandle);
         }
 
         const handleSlideChange = () => {
             const newIndex = swiper.realIndex;
 
             setCurrentSlideIndex(newIndex);
-            setProductLinkHref(data[newIndex].attributes.urlHandle);
 
             localStorage.setItem('swiperIndex', newIndex);
         };
@@ -52,22 +49,21 @@ export default function HomeProductSlider({ data }) {
         <>
             <swiper-container ref={sliderRef} class="grid w-full fixed z-10" slides-per-view="1.9" centered-slides="true" loop>
                 {data.map((item, index) => 
-                    <swiper-slide class="flex items-center h-screen" key={index}>
-                        <Image unoptimized className="w-[85%] lg:w-[80%] xl:w-[75%] 2xl:w-[70%] max-w-2xl mx-auto" priority src={`http://localhost:1337${item.attributes.image.data.attributes.url}`} width={700} height={700} alt=""/>
+                    <swiper-slide class="flex items-center h-screen relative z-50" key={index}>
+                        <Link className="w-[85%] lg:w-[80%] xl:w-[75%] 2xl:w-[70%] max-w-2xl mx-auto" href={`product/${item.attributes.urlHandle}`}>
+                            <Image unoptimized className="w-[85%] lg:w-[80%] xl:w-[75%] 2xl:w-[70%] max-w-2xl mx-auto" priority src={`http://localhost:1337${item.attributes.image.data.attributes.url}`} width={700} height={700} alt=""/>
+                        </Link>
                     </swiper-slide>
                 )}
             </swiper-container>
-            <div ref={controlsRef} className="hidden lg:block z-10 fixed inset-0 group" onMouseMove={updateCursorPosition}>
-                <button aria-label="Prev slide" className="cursor-none fixed w-1/2 left-0 top-0 h-full rotate-180" onClick={() => sliderRef.current.swiper.slidePrev()}></button>
-                <button aria-label="Next slide" className="cursor-none fixed w-1/2 right-0 top-0 h-full" onClick={() => sliderRef.current.swiper.slideNext()}></button>
+            <div ref={controlsRef} className="hidden lg:block group" onMouseMove={updateCursorPosition}>
+                <button aria-label="Prev slide" className="cursor-none z-10 fixed w-1/3 left-0 top-0 h-full rotate-180" onClick={() => sliderRef.current.swiper.slidePrev()}></button>
+                <button aria-label="Next slide" className="cursor-none z-10 fixed w-1/3 right-0 top-0 h-full" onClick={() => sliderRef.current.swiper.slideNext()}></button>
                 <div style={{ ...cursorPosition }} className="hidden group-hover:block absolute -z-10 font-semibold text-5xl tracking-tighter min-w-40">{currentSlideIndex + 1} / {data.length}</div>
             </div>
             <div className="flex flex-col gap-4 fixed z-40 bottom-0 right-0 p-4">
                 <div className=" max-lg:w-full flex items-center justify-between">
                     <div className="font-semibold text-lg tracking-tighter text-end lg:hidden">{currentSlideIndex + 1} / {data.length}</div>
-                    {productLinkHref !== null && <Link href={`product/${productLinkHref}`} className="font-semibold py-2 px-8 bg-black ml-auto text-white">
-                        View product
-                    </Link>}
                 </div>
                 <h1 className="font-semibold text-xs lg:text-sm">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
             </div>
