253d11dc16dfb11825947e46384aedb25462b113
naiiiden <na4ko359@gmail.com>
2024-07-28 00:00:46 +0300

    swiper only active slide linkis clickable

--- components/HomeProductSlider/HomeProductSlider.js
+++ components/HomeProductSlider/HomeProductSlider.js
@@ -56,7 +56,7 @@ export default function HomeProductSlider({ data }) {
         <div className="group cursor-none" onMouseMove={updateCursorPosition}>
             <swiper-container ref={sliderRef} class="grid w-full fixed z-10" slides-per-view="1.9" centered-slides="true" loop slide-to-clicked-slide="true">
                 {data.map((item, index) => 
-                    <swiper-slide class="flex items-center h-screen" key={index}>
+                    <swiper-slide class="flex items-center h-screen swiper-slide" key={index}>
                         <Link href={`product/${item.attributes.urlHandle}`} className="w-[85%] lg:w-[80%] xl:w-[75%] 2xl:w-[70%] max-w-2xl mx-auto">
                             <Image unoptimized priority src={`http://localhost:1337${item.attributes.image.data.attributes.url}`} width={700} height={700} alt=""/>
                         </Link>
@@ -67,9 +67,6 @@ export default function HomeProductSlider({ data }) {
             <div className="flex flex-col gap-4 fixed z-40 bottom-0 right-0 p-4">
                 <div className=" max-lg:w-full flex items-center justify-between">
                     <div className="font-semibold text-lg tracking-tighter text-end lg:hidden">{currentSlideIndex + 1} / {data.length}</div>
-                    {productLinkHref !== null && <Link href={`product/${productLinkHref}`} className="font-semibold py-2 px-8 bg-black ml-auto text-white">
-                        View product
-                    </Link>}
                 </div>
                 <h1 className="font-semibold text-xs lg:text-sm">An exquisite collection of contemporary furniture designs. All images rights belong to Google’s Arts and Culture and their respective owners.</h1>
             </div>
