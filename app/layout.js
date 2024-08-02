2ffc767700ab85a41f14378e6c00256be146a3a7
naiiiden <na4ko359@gmail.com>
2024-08-02 22:41:39 +0300

    lenislayout put in folder

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
-import LenisLayout from "@/components/LenisLayout";
+import LenisLayout from "@/components/LenisLayout/LenisLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
