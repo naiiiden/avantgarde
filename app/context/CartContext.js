2901de033fba13b328f9033874c50e2946689957
naiiiden <na4ko359@gmail.com>
2024-08-03 08:26:18 +0300

    addtocart func

--- app/context/CartContext.js
+++ app/context/CartContext.js
@@ -5,8 +5,12 @@ export const CartContext = createContext();
 export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
 
+    const addToCart = (item) => {
+        setCart((prevCart) => [...prevCart, item]);
+    }
+
     return (
-        <CartContext.Provider value={{ cart, setCart }}>
+        <CartContext.Provider value={{ cart, addToCart }}>
             {children}
         </CartContext.Provider>
     )
--- components/AddToCartButton/AddToCartButton.js
+++ components/AddToCartButton/AddToCartButton.js
@@ -3,11 +3,11 @@ import { CartContext, CartProvider } from "@/app/context/CartContext";
 import { useContext, useState } from "react";
 
 export default function AddToCartButton({ productToAdd }) {
-    const { cart, setCart } = useContext(CartContext);
+    const { cart, addToCart } = useContext(CartContext);
 
     console.log(1, cart);
 
     return (
-        <button onClick={() => setCart([...cart, productToAdd])} className="mt-4 font-semibold bg-black text-white w-full p-2 uppercase">Add to cart</button>
+        <button onClick={() => addToCart(productToAdd)} className="mt-4 font-semibold bg-black text-white w-full p-2 uppercase">Add to cart</button>
     ) 
 }
