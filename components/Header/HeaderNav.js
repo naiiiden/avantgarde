a7355664ac5a10939e21df10cefce81aa5549e56
naiiiden <na4ko359@gmail.com>
2024-08-06 00:34:18 +0300

    headernav fix

--- components/Header/HeaderNav.js
+++ components/Header/HeaderNav.js
@@ -13,7 +13,7 @@ export default function HeaderNav({ data }) {
             <ul className="text-end flex gap-4">
                 {data.attributes['Header'].links.map((link) => 
                     <li key={link.text}>
-                        <Link href={`/${link.url}`} className={`${pathname === `/${link.url}` ? 'underline italic' : 'hover:underline'} decoration-2 leading-none`}>
+                        <Link href={`/${link.url}`} className={`${pathname === `/${link.url}` ? 'underline italic' : 'hover:underline'} decoration-2 leading-none inline-flex`}>
                             {link.text} {link.text.toLowerCase() === "cart" && <span className="before:content-['('] after:content-[')'] inline-flex">{cart.length}</span>}
                         </Link>
                     </li>
