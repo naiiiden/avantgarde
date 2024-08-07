fea3223647a71d6fa64c63120013618c2ff62333
naiiiden <na4ko359@gmail.com>
2024-08-08 00:09:40 +0300

    addtocartbutton update

--- components/AddToCartButton/AddToCartButton.js
+++ components/AddToCartButton/AddToCartButton.js
@@ -1,13 +1,26 @@
 "use client";
 import { CartContext } from "@/app/context/CartContext";
-import { useContext } from "react";
+import Link from "next/link";
+import { useContext, useState, useEffect } from "react";
 
 export default function AddToCartButton({ productToAdd }) {
     const { cart, addToCart } = useContext(CartContext);
+    const [buttonText, setButtonText] = useState("Add to cart");
 
-    console.log(1, cart);
+    useEffect(() => {
+        const isProductInCart = cart.some(item => item.id === productToAdd.id);
 
-    return (
-        <button onClick={() => addToCart(productToAdd)} className="mt-4 font-semibold bg-black text-white w-full p-2 uppercase">Add to cart</button>
-    ) 
+        if (isProductInCart) {
+            setButtonText("Go to cart");
+        } else {
+            setButtonText("Add to cart");
+        }
+    }, [cart, productToAdd]);
+
+    return buttonText === "Add to cart" ? 
+        <button onClick={() => addToCart(productToAdd)} className="mt-4 font-semibold bg-black text-white w-full p-2 uppercase">
+                {buttonText}
+        </button>
+        :    
+        <Link className="block text-center mt-4 font-semibold bg-black text-white w-full p-2 uppercase" href="/cart">{buttonText}</Link>
 }
