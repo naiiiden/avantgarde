10b95c0951a4062d22b3b802beaccac0f9025020
naiiiden <na4ko359@gmail.com>
2024-08-08 23:18:10 +0300

    some progress

--- app/api/checkout_sessions/route.js
+++ app/api/checkout_sessions/route.js
@@ -1,27 +1,29 @@
+import { NextResponse } from 'next/server';
 import stripe from 'stripe';
 
 const stripeInstance = stripe(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      const session = await stripeInstance.checkout.sessions.create({
+export async function POST(req) {
+  try {
+    const { line_items } = await req.json();
+    const session = await stripeInstance.checkout.sessions.create({
         line_items: [
-          {
-            price: 'price_1PlFga2M4rj1gd0ux9LsirbP',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
-      res.redirect(303, session.url);
-    } catch (err) {
-      res.status(err.statusCode || 500).json({ error: err.message });
-    }
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+            {
+              // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+              price: 'price_1PlFga2M4rj1gd0ux9LsirbP',
+              quantity: 1,
+            },
+          ],
+      mode: 'payment',
+      success_url: `${req.headers.get('origin')}/?success=true`,
+      cancel_url: `${req.headers.get('origin')}/?canceled=true`,
+    });
+    return NextResponse.redirect(session.url, 303);
+  } catch (err) {
+    return NextResponse.json({ error: err.message }, { status: err.statusCode || 500 });
   }
+}
+
+export function GET() {
+  return NextResponse.json({ error: 'Method Not Allowed' }, { status: 405 });
 }
