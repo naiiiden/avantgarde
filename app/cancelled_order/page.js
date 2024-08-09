55d2fb4a3dab7e8e9a586de1151d3c4a8044a9c8
naiiiden <na4ko359@gmail.com>
2024-08-09 16:36:01 +0300

    center text

--- app/cancelled_order/page.js
+++ app/cancelled_order/page.js
@@ -5,7 +5,7 @@ export const metadata = {
 
 export default async function Page() {
     return (
-        <main className="px-4 pb-4 max-w-5xl ml-auto">
+        <main className="px-4 pb-4 max-w-5xl mx-auto">
             <h1>Cancelled order.</h1>
         </main>
     )
