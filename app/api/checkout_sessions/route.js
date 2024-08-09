150a377474c319cfad16530e1eee1843bd13c686
naiiiden <na4ko359@gmail.com>
2024-08-09 23:50:59 +0300

    successful page

--- app/api/checkout_sessions/route.js
+++ app/api/checkout_sessions/route.js
@@ -15,7 +15,7 @@ export async function POST(req) {
         },
       ],
       mode: 'payment',
-      success_url: `${origin}/successful_order`,
+      success_url: `${origin}/successful_order?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${origin}/cancelled_order?session_id={CHECKOUT_SESSION_ID}`,
     });
 
