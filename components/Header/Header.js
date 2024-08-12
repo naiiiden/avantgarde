54528e6c584a3cec2a13e7d93852c1f51125fc62
naiiiden <na4ko359@gmail.com>
2024-08-12 20:55:00 +0300

    header links capitalize instead of uppercase

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -7,7 +7,7 @@ export default async function Header() {
     const data = await getData('http://localhost:1337/api/global?populate[Header][populate]=*');
 
     return (
-        <header className={`sticky z-20 top-0 p-4 font-semibold flex flex-wrap justify-between gap-3 w-full uppercase`}>
+        <header className={`sticky z-20 top-0 p-4 font-semibold flex flex-wrap justify-between gap-3 w-full capitalize`}>
             <Link href="/" className={`w-full min-w-40 transition-all duration-300 hover:opacity-85`}>
                 <Image className="w-full" priority src={`http://localhost:1337${data.data.attributes['Header']['Image'].data.attributes.url}`} width={1} height={1} alt={data.data.attributes['Header']['Image'].data.attributes.alternativeText}/>
             </Link>
