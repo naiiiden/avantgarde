49b7e2054daedaf1f94a72755f386de6c4fe24e2
naiiiden <na4ko359@gmail.com>
2024-08-15 21:27:42 +0300

    switch pages

---
+++ app/home/page.js
@@ -0,0 +1,13 @@
+import HomeProductSilder from "@/components/HomeProductSlider/HomeProductSlider";
+import { getData } from "../utilities/getData";
+
+export default async function Home() {
+  const data = await getData('http://localhost:1337/api/products?populate=image&pagination[pageSize]=99');
+
+  return (
+    <main>
+      <h1 className="sr-only">An exquisite collection of contemporary furniture designs.</h1>
+      <HomeProductSilder data={data.data}/>
+    </main>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
-import HomeProductSilder from "@/components/HomeProductSlider/HomeProductSlider";
-import { getData } from "./utilities/getData";
-
-export default async function Home() {
-  const data = await getData('http://localhost:1337/api/products?populate=image&pagination[pageSize]=99');
-
+export default function Page() {
   return (
-    <main>
-      <h1 className="sr-only">An exquisite collection of contemporary furniture designs.</h1>
-      <HomeProductSilder data={data.data}/>
-    </main>
+      <div className="fixed inset-0 z-50 bg-[#0c0c0c] text-white font-bold">
+          <h1>Avantgarde is an exquisite collection of carefully curated contemporary furniture designs.</h1>
+      </div>
   )
 }
