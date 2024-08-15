1740aa4096767e5305c4ec4d902cda878d37fabd
naiiiden <na4ko359@gmail.com>
2024-08-15 21:52:36 +0300

    test

---
+++ components/Test.js
@@ -0,0 +1,17 @@
+"use client";
+import { useState } from "react";
+
+export default function Test() {
+    const [splashViewed, setSplashViewed] = useState(sessionStorage.getItem("splashShown"));
+
+    function closeSplash() {
+        sessionStorage.setItem("splashShown", "true");
+        setSplashViewed(true);
+    }
+
+    return !splashViewed && (
+        <div className="fixed inset-0 z-50 bg-[#0c0c0c] text-white font-bold" onClick={closeSplash}>
+            <h1>Avantgarde is an exquisite collection of carefully curated contemporary furniture designs.</h1>
+        </div>
+    ) 
+}
