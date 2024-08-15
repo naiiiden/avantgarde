e48b25cef2c903ee590339d19564ab63bb6be2b9
naiiiden <na4ko359@gmail.com>
2024-08-15 23:04:36 +0300

    delete home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
-export default function Page() {
+import HomeProductSilder from "@/components/HomeProductSlider/HomeProductSlider";
+import { getData } from "./utilities/getData";
+import Test from "@/components/Test";
+
+export default async function Home() {
+  const data = await getData('http://localhost:1337/api/products?populate=image&pagination[pageSize]=99');
+
   return (
-      <div className="fixed inset-0 z-50 bg-[#0c0c0c] text-white font-bold">
-          <h1>Avantgarde is an exquisite collection of carefully curated contemporary furniture designs.</h1>
-      </div>
+    <>
+        <Test/>
+        <main>
+        <h1 className="sr-only">An exquisite collection of contemporary furniture designs.</h1>
+        <HomeProductSilder data={data.data}/>
+        </main>
+    </>
   )
 }
