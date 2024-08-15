5495030afb78d9e732d6a1308b633b908c6234db
naiiiden <na4ko359@gmail.com>
2024-08-15 16:47:16 +0300

    revert

--- app/api/welcome_page_cookie/route.js
+++
@@ -1,6 +0,0 @@
-import { cookies } from 'next/headers'
-
-export async function POST() {
-    cookies().set('splashShown', 'true');
-    return new Response('Cookie set', { status: 200 });
-}
--- app/welcome/page.js
+++ app/welcome/page.js
@@ -1,20 +1,6 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
 export default function Page() {
-    const router = useRouter();
-
-    useEffect(() => {
-        sessionStorage.setItem("splashShown", "true");
-    }, []);
-
-    const handleClose = () => {
-        router.push('/');
-    };
-
     return (
-        <div className="fixed inset-0 z-50 bg-[#0c0c0c] text-white font-bold" onClick={handleClose}>
+        <div className="fixed inset-0 z-50 bg-[#0c0c0c] text-white font-bold">
             <h1>Avantgarde is an exquisite collection of carefully curated contemporary furniture designs.</h1>
         </div>
     )
