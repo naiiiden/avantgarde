e8b2522b203b2ef4a4b2668a7c8e704ac221d53f
naiiiden <na4ko359@gmail.com>
2024-08-15 23:29:53 +0300

    splashscreen component

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 import HomeProductSilder from "@/components/HomeProductSlider/HomeProductSlider";
+import SplashScreen from "@/components/SplashScreen/SplashScreen";
 import { getData } from "./utilities/getData";
-import Test from "@/components/Test";
 
 export default async function Home() {
   const data = await getData('http://localhost:1337/api/products?populate=image&pagination[pageSize]=99');
 
   return (
     <>
-        <Test/>
+        <SplashScreen/>
         <main>
         <h1 className="sr-only">An exquisite collection of contemporary furniture designs.</h1>
         <HomeProductSilder data={data.data}/>
