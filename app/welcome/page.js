684a0c24c0d2414441e7062efc1459e62a30cefe
naiiiden <na4ko359@gmail.com>
2024-08-15 13:51:03 +0300

    welcome page

---
+++ app/welcome/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Page() {
+    return (
+        <div className="fixed inset-0 z-50 bg-[#0c0c0c] text-white font-bold">
+            <h1>Avantgarde is an exquisite collection of carefully curated contemporary furniture designs.</h1>
+        </div>
+    )
+}
