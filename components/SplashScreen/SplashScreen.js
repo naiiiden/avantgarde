9dd04508b759b8c6059e71da5afa2852ff106c1e
naiiiden <na4ko359@gmail.com>
2024-08-16 16:35:42 +0300

    splash page a11y improvements

--- components/SplashScreen/SplashScreen.js
+++ components/SplashScreen/SplashScreen.js
@@ -10,9 +10,9 @@ export default function SplashScreen() {
     }
 
     return (
-        <div className={`fixed grid items-center justify-end inset-0 z-50 bg-[#0c0c0c] text-white font-bold ${!splashViewed ? '' : '-translate-y-full'} transition-all duration-1000`}>
+        <div onBlur={closeSplash} className={`fixed grid items-center justify-end inset-0 z-50 bg-[#0c0c0c] text-white font-bold ${!splashViewed ? '' : '-translate-y-full'} transition-all duration-1000`}>
             <div className="fixed inset-0 z-30 cursor-pointer" onClick={closeSplash}></div>
-            <div className="p-4 relative z-40 text-2xl md:text-3xl max-w-2xl" aria-hidden="true">Avantgarde is an exquisite collection of carefully curated contemporary furniture designs and a fictional store. All images rights belong to <a href="https://artsandculture.google.com/" target="_blank">Google’s Arts and Culture</a> and their respective owners.</div>
+            <div className="p-4 relative z-40 text-2xl md:text-3xl max-w-2xl" aria-hidden="true">Avantgarde is an exquisite collection of carefully curated contemporary furniture designs and a fictional store. All images rights belong to <a href="https://artsandculture.google.com/" target="_blank" tabIndex={splashViewed ? -1 : 1}>Google’s Arts and Culture</a> and their respective owners.</div>
         </div>
     ) 
 }
