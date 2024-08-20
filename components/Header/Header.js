d89188a14a32ef23146b832aefe10d5c225a2ca3
naiiiden <na4ko359@gmail.com>
2024-08-20 18:09:13 +0300

    header logo revert opacity

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -8,7 +8,7 @@ export default async function Header() {
 
     return (
         <header className={`sticky z-20 top-0 p-4 flex flex-wrap justify-between gap-3 w-full capitalize`}>
-            <Link href="/" className={`w-full min-w-40 transition-all duration-300 hover:opacity-85`}>
+            <Link href="/" className={`w-full min-w-40 transition-all duration-300 opacity-[.825] hover:opacity-100`}>
                 <Image className="w-full" priority src={`http://localhost:1337${data.data.attributes['Header']['Image'].data.attributes.url}`} width={1} height={1} alt={data.data.attributes['Header']['Image'].data.attributes.alternativeText}/>
             </Link>
             <HeaderNav data={data.data}/>
