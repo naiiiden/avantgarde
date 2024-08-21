2caeb29c66532910f9f09c507782e10ae6de33c6
naiiiden <na4ko359@gmail.com>
2024-08-21 09:17:20 +0300

    update img width product

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1700px",
+      },
+    },
   },
   plugins: [],
 };
