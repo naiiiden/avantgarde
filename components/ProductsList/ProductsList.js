271693b9e66a96893a31ffe8662dfced740e0e65
naiiiden <na4ko359@gmail.com>
2024-08-26 21:15:22 +0300

    productslist img update

--- components/ProductsList/ProductsList.js
+++ components/ProductsList/ProductsList.js
@@ -43,7 +43,7 @@ export default function ProductsList({ searchParams, data }) {
                         <li key={index} className="relative">
                             <Link className="group" href={`product/${item.attributes.urlHandle}`}>
                                 {item.attributes?.image?.data?.attributes?.url ? (
-                                        <Image className="group-hover:opacity-[.0225] group-focus-within:opacity-[.0225] transition-opacity duration-500" unoptimized src={`http://localhost:1337${item.attributes.image.data.attributes.url}`} priority width={1000} height={1000} alt=""/>
+                                        <Image className="group-hover:opacity-[.0225] group-focus-within:opacity-[.0225] transition-opacity duration-500 w-full" unoptimized src={`http://localhost:1337${item.attributes.image.data.attributes.url}`} priority width={1500} height={1500} alt=""/>
                                     ) : (
                                         <div className="text-xs min-h-64 grid place-content-center text-center group-hover:opacity-[.0225] group-focus-within:opacity-[.0225] transition-opacity duration-500">
                                             <svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#0c0c0c"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z"/></svg>
