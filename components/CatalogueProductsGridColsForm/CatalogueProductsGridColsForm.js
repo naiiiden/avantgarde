f39236d47ccedecd1429f428b1bdf53f178a9f2e
naiiiden <na4ko359@gmail.com>
2024-08-26 23:02:59 +0300

    fix checked

--- components/CatalogueProductsGridColsForm/CatalogueProductsGridColsForm.js
+++ components/CatalogueProductsGridColsForm/CatalogueProductsGridColsForm.js
@@ -17,22 +17,22 @@ export default function CatalogueProductsGridColsForm({ currentView }) {
         <form className="pb-4 flex flex-wrap gap-x-2 justify-end">
             <p className="font-semibold">View:</p>
             <label className="relative">
-                <input checked={currentView === "grid"} onChange={(e) => handleSort(e.target.id)} name="view" id="2" className="peer opacity-0 w-full h-full absolute cursor-pointer" type="radio"/>
+                <input checked={currentView === "2"} onChange={(e) => handleSort(e.target.id)} name="view" id="2" className="peer opacity-0 w-full h-full absolute cursor-pointer" type="radio"/>
                 <span className="peer-checked:underline peer-hover:underline decoration-2 peer-focus:outline">2</span>
                 <span aria-hidden="true">,</span>
             </label>
             <label className="relative">
-                <input checked={currentView === "index"} onChange={(e) => handleSort(e.target.id)} name="view" id="3" className="peer opacity-0 w-full h-full absolute cursor-pointer" type="radio"/>
+                <input checked={currentView === "3"} onChange={(e) => handleSort(e.target.id)} name="view" id="3" className="peer opacity-0 w-full h-full absolute cursor-pointer" type="radio"/>
                 <span className="peer-checked:underline peer-hover:underline decoration-2 peer-focus:outline">3</span>
                 <span aria-hidden="true">,</span>
             </label>
             <label className="relative">
-                <input checked={currentView === "grid"} onChange={(e) => handleSort(e.target.id)} name="view" id="4" className="peer opacity-0 w-full h-full absolute cursor-pointer" type="radio"/>
+                <input checked={currentView === "4"} onChange={(e) => handleSort(e.target.id)} name="view" id="4" className="peer opacity-0 w-full h-full absolute cursor-pointer" type="radio"/>
                 <span className="peer-checked:underline peer-hover:underline decoration-2 peer-focus:outline">4</span>
                 <span aria-hidden="true">,</span>
             </label>
             <label className="relative">
-                <input checked={currentView === "grid"} onChange={(e) => handleSort(e.target.id)} name="view" id="5" className="peer opacity-0 w-full h-full absolute cursor-pointer" type="radio"/>
+                <input checked={currentView === "5"} onChange={(e) => handleSort(e.target.id)} name="view" id="5" className="peer opacity-0 w-full h-full absolute cursor-pointer" type="radio"/>
                 <span className="peer-checked:underline peer-hover:underline decoration-2 peer-focus:outline">5</span>
                 <span aria-hidden="true"></span>
             </label>
