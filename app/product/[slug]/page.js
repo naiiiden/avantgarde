344dbea61fa6f6238ae286c7a8e960e8fc0a066e
naiiiden <na4ko359@gmail.com>
2024-08-30 18:18:51 +0300

    product img update width

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -42,7 +42,7 @@ export default async function Page({ params }) {
     return (
         <main className="lg:px-0 flex flex-col gap-4 lg:flex-row lg:pb-0 mt-auto">
             {currentProduct.data[0].attributes?.image?.data?.attributes?.url ? (
-                <Image blurDataURL={`http://localhost:1337${currentProduct.data[0].attributes.image.data.attributes.url}`} placeholder="blur" unoptimized className="w-auto lg:max-w-[40%] 2xl:max-w-5xl 4xl:max-w-6xl" priority src={`http://localhost:1337${currentProduct.data[0].attributes.image.data.attributes.url}`} width={2000} height={2000} alt=""/>
+                <Image blurDataURL={`http://localhost:1337${currentProduct.data[0].attributes.image.data.attributes.url}`} placeholder="blur" unoptimized className="w-auto lg:max-w-[40%] 2xl:max-w-4xl 3xl:max-w-5xl 4xl:max-w-6xl" priority src={`http://localhost:1337${currentProduct.data[0].attributes.image.data.attributes.url}`} width={2000} height={2000} alt=""/>
             ) : (
                 <div className="text-center text-xs w-[85%] lg:w-[80%] xl:w-[75%] 2xl:w-[70%] max-w-2xl mx-auto">
                     <svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" height="56px" viewBox="0 -960 960 960" width="56px" fill="#0c0c0c"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z"/></svg>
