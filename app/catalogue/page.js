7ae3370974d0bd6bd23da298e834ae8c962030a6
naiiiden <na4ko359@gmail.com>
2024-09-02 12:16:45 +0300

    catalogue custom cursor

--- app/catalogue/page.js
+++ app/catalogue/page.js
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import CatalogueProductsViewForm from '@/components/CatalogueProductsViewForm/CatalogueProductsViewForm';
 import CatalogueSortProductsForm from '@/components/CatalogueSortProductsForm/CatalogueSortProductsForm';
 import { getData } from '../utilities/getData';
+import CustomCursor from '@/components/CustomCursor/CustomCursor';
 
 export const metadata = {
     title: "Catalogue | Avantgarde",
@@ -54,7 +55,7 @@ export default async function Page({ searchParams }) {
                 <ul style={searchParams.cols < 6 ? { gridTemplateColumns: `repeat(${searchParams.cols}, minmax(0, 1fr))` } : undefined} className="products-list-reveal grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                     {sortedData.map((item, index) =>
                         <li key={index} className="relative">
-                            <Link className="group" href={`product/${item.attributes.urlHandle}/?sortBy=${searchParams.sortBy}`}>
+                            <Link data-cursor-text className="cursor-none group" href={`product/${item.attributes.urlHandle}/?sortBy=${searchParams.sortBy}`}>
                                 {item.attributes?.image?.data?.attributes?.url ? (
                                         <Image className="group-hover:opacity-[.0225] group-focus-within:opacity-[.0225] transition-opacity duration-500 w-full" unoptimized src={`http://localhost:1337${item.attributes.image.data.attributes.url}`} priority width={1500} height={1500} alt=""/>
                                     ) : (
@@ -92,7 +93,7 @@ export default async function Page({ searchParams }) {
                     <ul className="products-list-reveal grid">
                         {sortedData.map((item, index) =>
                             <li key={index} className="border-b last:border-b-0 border-black transition-all duration-500 group">
-                                <Link className="flex py-2 text-sm" href={`product/${item.attributes.urlHandle}/?sortBy=${searchParams.sortBy}`}>
+                                <Link data-cursor-text className="cursor-none flex py-2 text-sm" href={`product/${item.attributes.urlHandle}/?sortBy=${searchParams.sortBy}`}>
                                     {item.attributes?.image?.data?.attributes?.url ? (
                                         <Image unoptimized className="w-full max-w-24 sm:max-w-32 lg:max-w-40 group-hover:max-w-28 group-hover:sm:max-w-44 group-hover:lg:max-w-56 group-hover:xl:max-w-60 group-focus-within:max-w-28 group-focus-within:sm:max-w-44 group-focus-within:lg:max-w-56 group-focus-within:xl:max-w-60 transition-all duration-500" src={`http://localhost:1337${item.attributes.image.data.attributes.url}`} priority width={200} height={200} alt="" />
                                     ) : (
@@ -114,6 +115,7 @@ export default async function Page({ searchParams }) {
                     </ul>
                 </>
             }
+            <CustomCursor hoverText="View product"/>
         </main>
     )
 }
---
+++ components/CustomCursor/CustomCursor.js
@@ -0,0 +1,56 @@
+"use client";
+import { useState, useEffect } from "react";
+
+export default function CustomCursor({ hoverText }) {
+    const [cursorPosition, setCursorPosition] = useState({ top: -100, left: 0 });
+    const [isHovering, setIsHovering] = useState(false);
+
+    const updateCursorPosition = (e) => {
+        setCursorPosition({ top: `${e.clientY - 14}px`, left: `${e.clientX - 10}px` });
+    };
+
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            if (isHovering) {
+                updateCursorPosition(e);
+            }
+        };
+
+        const handleMouseEnter = () => setIsHovering(true);
+        const handleMouseLeave = () => setIsHovering(false);
+
+        const attachListeners = () => {
+            const linkElements = document.querySelectorAll("[data-cursor-text]");
+            linkElements.forEach(link => {
+                link.addEventListener("mouseenter", handleMouseEnter);
+                link.addEventListener("mouseleave", handleMouseLeave);
+            });
+        };
+
+        const observer = new MutationObserver(() => {
+            attachListeners();
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+
+        document.addEventListener("mousemove", handleMouseMove);
+        attachListeners();
+
+        return () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            observer.disconnect();
+        };
+    }, [isHovering]);
+
+    return (
+        <div
+            style={{ ...cursorPosition }}
+            className={`${isHovering ? "block" : "hidden"} uppercase fixed pointer-events-none select-none z-10 font-bold text-4xl tracking-tighter min-w-40`}
+        >
+            <span className="-ml-28">{hoverText}</span>
+        </div>
+    );
+}
